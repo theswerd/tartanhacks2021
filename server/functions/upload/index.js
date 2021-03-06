993bb53023464dba4295a237a01d810828e61dd9
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-03-06 10:42:46 -0800

    hows this

--- server/functions/upload/index.js
+++ server/functions/upload/index.js
@@ -1,5 +1,26 @@
-exports.upload = (req, res) => {
-    let message = req.query.message || req.body.message || 'Deployment worked!';
-    res.status(200).send(message);
-  };
-  
+const express = require('express');
+
+const { filesUpload } = require('./middleware');
+
+
+app = express();
+
+app.get('/upload', filesUpload, function(req, res) {
+  res.send('Alr now its express')
+  // will contain all text fields
+  // req.body 
+  // // will contain an array of file objects
+  // /*
+  //   {
+  //     fieldname: 'image',       String - name of the field used in the form
+  //     originalname,             String - original filename of the uploaded image
+  //     encoding,                 String - encoding of the image (e.g. "7bit")
+  //     mimetype,                 String - MIME type of the file (e.g. "image/jpeg")
+  //     buffer,                   Buffer - buffer containing binary data
+  //     size,                     Number - size of buffer in bytes
+  //   }
+  // */
+  // req.files 
+})
+
+exports = app;
