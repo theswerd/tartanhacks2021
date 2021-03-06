f1430e0a3243efc3c0bfb6c124f5e53d36841e94
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-03-06 12:41:53 -0800

    whmmmmm

--- server/functions/upload/index.js
+++ server/functions/upload/index.js
@@ -62,6 +62,7 @@ app.post("/", upload.single("file"), (req, res) => {
           headers: response.headers,
           status: response.status,
           body: await response.text(),
+          imageURL: publicUrl
         });
       }
     });
